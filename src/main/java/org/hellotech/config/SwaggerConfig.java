441c07df6de423e92bbbb9693de6cab2e4d74c40
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2022-01-27 16:31:51 +0530

    Customize Swagger Info

--- src/main/java/org/hellotech/config/SwaggerConfig.java
+++ src/main/java/org/hellotech/config/SwaggerConfig.java
@@ -3,7 +3,9 @@ package org.hellotech.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,8 +16,15 @@ public class SwaggerConfig {
 
     @Bean
     public Docket getSwaggerAPI(){
-        return new Docket(DocumentationType.SWAGGER_2).groupName("Restaurant APIs")
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(getApiInfo()).groupName("Restaurant APIs")
                 .select().apis(RequestHandlerSelectors.basePackage("org.hellotech.controller"))
+                .build();
+    }
+
+    private ApiInfo getApiInfo(){
+        return new ApiInfoBuilder().title("Restaurant Base")
+                .description("Ref Guide")
+                .license("MITT")
                 .build();
     }
 
