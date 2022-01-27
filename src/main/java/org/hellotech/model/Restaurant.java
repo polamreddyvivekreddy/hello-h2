633ebdf022a78ae7af97052dab05e867b41a3f92
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2022-01-27 12:54:24 +0530

    model for lombok

---
+++ src/main/java/org/hellotech/model/Restaurant.java
@@ -0,0 +1,18 @@
+package org.hellotech.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Restaurant {
+
+    private int id;
+    private String name;
+
+
+}
