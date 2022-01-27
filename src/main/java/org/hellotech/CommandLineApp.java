fbb3b2708c4eda75cb461671443ecd5325c8f4cd
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2022-01-27 12:48:14 +0530

    actuautor, custom mapping & lombok

--- src/main/java/org/hellotech/CommandLineApp.java
+++ src/main/java/org/hellotech/CommandLineApp.java
@@ -2,6 +2,7 @@ package org.hellotech;
 
 import org.hellotech.entity.Food;
 import org.hellotech.entity.Person;
+import org.hellotech.model.Restaurant;
 import org.hellotech.repository.FoodRepository;
 import org.hellotech.repository.PersonRepository;
 import org.slf4j.Logger;
@@ -16,19 +17,24 @@ public class CommandLineApp {
 
 
     public CommandLineApp(PersonRepository personRepository, FoodRepository foodRepository) {
-        Person person = new Person(1001,"Vivek",21,"18-02-1999");
+        Person person = new Person(1001, "Vivek", 21, "18-02-1999");
         personRepository.save(person);
 
-        foodRepository.save(new Food( "Dosa",person));
-        foodRepository.save( new Food( "Idli",person));
-        foodRepository.save(new Food( "Poori",person));
+        foodRepository.save(new Food("Dosa", person));
+        foodRepository.save(new Food("Idli", person));
+        foodRepository.save(new Food("Poori", person));
     }
 
     public static void main(String[] args) {
 
         SpringApplication.run(CommandLineApp.class);
-
         LOGGER.info("hello after spring run");
+
+        Restaurant restaurant = new Restaurant();
+        restaurant.setId(1);
+        restaurant.setName("Babai Hotel");
+        LOGGER.info("restaurant.getId() {}",restaurant.getId());
+        LOGGER.info("restaurant.getName() {}",restaurant.getName());
 
 
     }
