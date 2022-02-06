7d5ef886561ceddeebd46ba9821acaac7e0a9673
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2022-02-06 14:13:03 +0530

    Aspect Oriented Logging @Before @After

---
+++ src/main/java/org/hellotech/config/LoggingAspectConfig.java
@@ -0,0 +1,25 @@
+package org.hellotech.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+@Slf4j
+public class LoggingAspectConfig {
+
+    // pointCut expression accessModifier returnType packageName.className.methodName(..)
+    @Before("execution(public * org.hellotech.controller.*.*(..))")
+    public void logBeforeExecutingControllerPublicMethods(JoinPoint joinPoint) {
+        log.info("Triggered : {}", joinPoint.getSignature().getName());
+    }
+
+    @After("execution(public * org.hellotech.controller.*.*(..))")
+    public void logAfterExecutingControllerPublicMethods(JoinPoint joinPoint) {
+        log.info("Executed : {}", joinPoint.getSignature().getName());
+    }
+}
