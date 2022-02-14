10ab6cd72f93a70474a9a87a4c33c1f5c4702619
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2022-02-14 18:31:58 +0530

    Demo login without auto generation of password

---
+++ src/main/java/org/hellotech/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package org.hellotech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+// Prevents auto generation of session password
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("vivek")
+        .password("strongpassword").roles("ADMIN");
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
